21c526f04ad805444ba69e233be9a4ce97845b9f
wangxing <sampras.thu@gmail.com>
2020-08-02 20:06:18 +0000

    add pagination

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -40,13 +40,22 @@ var geocoder = NodeGeocoder(options);
 
 // INDEX route - show all campgrounds
 router.get("/campgrounds", function(req, res){
+	var perPage = 8;
+    var pageQuery = parseInt(req.query.page);
+    var pageNumber = pageQuery ? pageQuery : 1;
     // Get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds){
-    	if(err){
-           	console.log(err);
-       	} else {
-          	res.render("campgrounds/index", {campgrounds: allCampgrounds, page: "campgrounds"});
-       	}
+    Campground.find({}).skip((perPage * pageNumber) - perPage).limit(perPage).exec(function (err, allCampgrounds) {
+        Campground.count().exec(function (err, count) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("campgrounds/index", {
+                    campgrounds: allCampgrounds,
+                    current: pageNumber,
+                    pages: Math.ceil(count / perPage)
+                });
+            }
+        });
     });
 });
 
